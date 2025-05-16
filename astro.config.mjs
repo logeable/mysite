4c469eb75c103c050a419e6cf8539dcc56720d58
logeable <logeable@gmail.com>
2025-05-16 12:57:47 +0800

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from "astro/config";
 
 import expressiveCode from "astro-expressive-code";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.logeable.com",
   integrations: [
     expressiveCode({
       themes: ["dracula"],
     }),
+    sitemap(),
   ],
 });
