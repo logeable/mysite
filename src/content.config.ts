be147e9e5c59d2ab3d64a73a68a617290020b408
logeable <logeable@gmail.com>
2025-05-16 14:52:29 +0800

    feat: add og image

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,15 +2,16 @@ import { glob } from "astro/loaders";
 import { z, defineCollection } from "astro:content";
 
 const blog = defineCollection({
-    loader: glob({ pattern: "**/[^_]*.md", base: "./src/data/blog/" }),
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        publishDate: z.date(),
-        tags: z.array(z.string()),
-        featured: z.boolean().optional(),
-    })
-})
-
+  loader: glob({ pattern: "**/[^_]*.md", base: "./src/data/blog/" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishDate: z.date(),
+    modifiedDate: z.date().optional(),
+    tags: z.array(z.string()),
+    featured: z.boolean().optional(),
+    ogImage: z.string().optional(),
+  }),
+});
 
 export const collections = { blog };
