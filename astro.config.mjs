49612f644459c0d26dbdae1e2afbcc1c7b74ff21
logeable <logeable@gmail.com>
2025-05-21 11:54:33 +0800

    chore: add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,12 @@ import expressiveCode from "astro-expressive-code";
 
 import sitemap from "@astrojs/sitemap";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.logeable.com",
-  integrations: [
-    expressiveCode({
-      themes: ["dracula"],
-    }),
-    sitemap(),
-  ],
+  integrations: [expressiveCode({
+    themes: ["dracula"],
+  }), sitemap(), mdx()],
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { glob } from "astro/loaders";
 import { z, defineCollection } from "astro:content";
 
 const blog = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.md", base: "./src/data/blog/" }),
+  loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/data/blog/" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
