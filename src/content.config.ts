c4f023cd8c4c7f9dab6b3dc3ade8ee3281c62dbd
logeable <logeable@gmail.com>
2026-04-12 01:51:20 +0800

    feat: support blog og images

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,17 +3,18 @@ import { z, defineCollection } from "astro:content";
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/data/blog/" }),
-  schema: z.object({
-    title: z.string(),
-    seoSlug: z.string().optional(),
-    description: z.string(),
-    publishDate: z.date(),
-    modifiedDate: z.date().optional(),
-    tags: z.array(z.string()),
-    featured: z.boolean().optional(),
-    ogImage: z.string().optional(),
-    isAI: z.boolean().optional().default(false),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      seoSlug: z.string().optional(),
+      description: z.string(),
+      publishDate: z.date(),
+      modifiedDate: z.date().optional(),
+      tags: z.array(z.string()),
+      featured: z.boolean().optional(),
+      ogImage: z.union([z.string(), image()]).optional(),
+      isAI: z.boolean().optional().default(false),
+    }),
 });
 
 export const collections = { blog };
